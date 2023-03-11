2230964b7423832d42d0df90a650a782aa8acf71
tharindu4829 <108778899+tharindu4829@users.noreply.github.com>
2023-03-11 08:05:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'UzBLUTlTNEs=',
+global.ownername = process.env.OWNER_NAME === undefined ? "Tharindu" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '9476173191' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
